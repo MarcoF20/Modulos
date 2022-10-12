b89507169577cd6904cc8ffa219e22e46e9f480c
MarcoF20 <piratafranco20@gmail.com>
2022-10-11 21:37:44 -0700

    Agregue main, zen372 y calculator

---
+++ zen372.py
@@ -0,0 +1,15 @@
+# Comentario 1 linea
+
+"""
+Comentario multilinea
+1 Linea
+2 Lineas
+3 ....
+"""
+print("Latosa1: No se")
+print("Latosa2: Mas vale pedir perdon que pedir permiso")
+print("Marco1: Camaron que se duerme se lo lleva la corriente")
+print("Marco2:Fuga")
+print("Derek: Fuga X 2")
+
+barba = "Algo, que sorpresa!"
